067796df0f226e411631ea7cda36c6f02d5b7eea
edwinfigueroa15 <edwinfigueroa15@hotmail.com>
2023-11-22 20:54:14 -0500

    Portafolio section 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import Components from './components/index';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ...Components],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/index.ts
@@ -0,0 +1,9 @@
+import { FooterComponent } from "./footer/footer.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { ParticleComponent } from "./particle/particle.component";
+
+export default [
+    NavBarComponent,
+    FooterComponent,
+    ParticleComponent
+]
