9465f81b521c25fc004e84fec8356513beb2736d
edwinfigueroa15 <edwinfigueroa15@hotmail.com>
2023-11-22 21:54:14 -0500

    Agregar particulas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import Components from './components/index';
+import ParticlesConfig from '../assets/json/particlesjs-config.json';
 
+declare var particlesJS: any;
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,5 +13,9 @@ import Components from './components/index';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'web-site';
+  ngOnInit() {
+    particlesJS('particles-js', ParticlesConfig, function () {
+      console.log('callback - particles.js config loaded');
+    });
+  }
 }
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,9 +1,7 @@
 import { FooterComponent } from "./footer/footer.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
-import { ParticleComponent } from "./particle/particle.component";
 
 export default [
     NavBarComponent,
-    FooterComponent,
-    ParticleComponent
+    FooterComponent
 ]
